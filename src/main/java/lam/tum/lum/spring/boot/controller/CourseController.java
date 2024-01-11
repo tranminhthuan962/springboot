b5f652fabae6324218d76ca7c02ec466a6726826
Minh Thuan <tranminhthuan962@gmail.com>
2024-01-11 14:44:58 +0700

    fix issue

--- src/main/java/lam/tum/lum/spring/boot/controller/CourseController.java
+++ src/main/java/lam/tum/lum/spring/boot/controller/CourseController.java
@@ -2,11 +2,13 @@ package lam.tum.lum.spring.boot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/course.api.com/v1")
 @Controller
 public class CourseController {
 
-    @GetMapping("/jspPage")
+    @GetMapping("/jspPages")
     public String printWelcomeJSP() {
         return "sayHello";
     }
--- src/main/java/lam/tum/lum/spring/boot/controller/CourseRestContrroller.java
+++ src/main/java/lam/tum/lum/spring/boot/controller/CourseRestContrroller.java
@@ -2,36 +2,35 @@ package lam.tum.lum.spring.boot.controller;
 
 import lam.tum.lum.spring.boot.model.Course;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@RequestMapping("course.api.com/v1")
 @RestController
 public class CourseRestContrroller {
-    @GetMapping("/course")
+    @GetMapping("/courses")
     public List<Course> printList() {
         return Arrays.asList(
                 new Course(1, "Java course", "Minh Thuan"),
                 new Course(2, "HTML course", "Thao Vi"),
                 new Course(3, "JS course", "Minh Thuan"),
                 new Course(4, "C# course", "Thao Vi")
-
         );
     }
 
-    @GetMapping("/printMessage")
-    @ResponseBody
+    @GetMapping("/homes")
     public String printMessage() {
+        StringBuilder sb = new StringBuilder();
 
-        StringBuffer sb = new StringBuffer();
         sb.append("<html>");
         sb.append("<head>");
         sb.append("<title> My first HTML</title>");
         sb.append("</head>");
         sb.append("<body>");
-        sb.append("My body");
+        sb.append("My first page");
         sb.append("</body>");
         sb.append("</html>");
 
